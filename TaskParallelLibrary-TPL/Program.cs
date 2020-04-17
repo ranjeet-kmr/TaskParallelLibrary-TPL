87f1769538cec0f07de2d36211508c242c5af15c
RANJEET KUMAR <ranjeet_kmr@hotmail.com>
2020-04-17 22:50:42 +0530

    Add project files.

---
+++ TaskParallelLibrary-TPL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibrary_TPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          var t1 =  Task.Factory.StartNew(() => impStuff(1,1000));
+          var t2 =  Task.Factory.StartNew(() => impStuff(2, 2500));
+          var t3 =  Task.Factory.StartNew(() => impStuff(3, 2000));
+
+            var tasklist = new List<Task> { t1, t2, t3 };
+           // Task.WaitAll(tasklist.ToArray());
+          var t4 =  Task.Factory.StartNew(() => otherimpStuff(1, 1000));
+          var t5 =   Task.Factory.StartNew(() => otherimpStuff(2, 2500));
+          var t6 =   Task.Factory.StartNew(() => otherimpStuff(3, 2000));
+
+            Console.WriteLine("Press any key to quit");
+            Console.Read();
+        }
+
+        static void  impStuff(int id , int sleepTime)
+        {
+            Console.WriteLine("Task {0} is Begining impStuff execution", id);
+            Thread.Sleep(sleepTime);
+            Console.WriteLine("Task {0} has completed impStuff execution", id);
+        }
+
+        static void otherimpStuff(int id, int sleepTime)
+        {
+            Console.WriteLine("Task {0} is Begining otherimpStuff execution", id);
+            Thread.Sleep(sleepTime);
+            Console.WriteLine("Task {0} has completed otherimpStuff execution", id);
+        }
+    }
+}
---
+++ TaskParallelLibrary-TPL/linqtest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibrary_TPL
+{
+    class linqtest
+    {
+        static void Main()
+        {
+            int[] ds = { 12, 21, 54, 65, 777, 77, 7, 3, 2, 1, 0, 453 };
+
+            var sa = from k in ds
+                     where k > 40
+                     orderby k descending
+                     select k;
+            foreach (var x in sa)
+      
+                Console.Write(x   + " ");
+                Console.ReadLine();
+
+
+        }
+
+
+    }
+}
